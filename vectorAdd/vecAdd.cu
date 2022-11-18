1e7fff80b936c8a28912ed0169c42e7c3eaabcc9
Paul Sathre <sath6220@vt.edu>
2022-11-18 11:24:36 -0800

    Fix: CUDA: If the kernel received less than nelem of work it would allocate less space, but try to run nelem items

--- vectorAdd/vecAdd.cu
+++ vectorAdd/vecAdd.cu
@@ -10,16 +10,17 @@ __global__ void vectorAddCUDA(REAL_TYPE *A, REAL_TYPE *B, REAL_TYPE *C, int32_t 
 extern "C" {
 void vecAdd(REAL_TYPE *A, REAL_TYPE *B, REAL_TYPE *C, int32_t lo, int32_t hi, int32_t nelem) {
   REAL_TYPE *dA, *dB, *dC;
+  int32_t work = hi-lo+1;
   cudaSetDevice(dev_num);
-  cudaMalloc(&dA, sizeof(REAL_TYPE) * (hi - lo + 1));
-  cudaMalloc(&dB, sizeof(REAL_TYPE) * (hi - lo + 1));
-  cudaMalloc(&dC, sizeof(REAL_TYPE) * (hi - lo + 1));
+  cudaMalloc(&dA, sizeof(REAL_TYPE) * work);
+  cudaMalloc(&dB, sizeof(REAL_TYPE) * work);
+  cudaMalloc(&dC, sizeof(REAL_TYPE) * work);
 
-  cudaMemcpy(dA, A + lo, sizeof(REAL_TYPE) * (hi - lo + 1), cudaMemcpyHostToDevice);
-  cudaMemcpy(dB, B + lo, sizeof(REAL_TYPE) * (hi - lo + 1), cudaMemcpyHostToDevice);
+  cudaMemcpy(dA, A + lo, sizeof(REAL_TYPE) * work, cudaMemcpyHostToDevice);
+  cudaMemcpy(dB, B + lo, sizeof(REAL_TYPE) * work, cudaMemcpyHostToDevice);
   dim3 block = {256, 1, 1};
-  dim3 grid = {(nelem / block.x) + (nelem % block.x ? 1 : 0), 1, 1};
-  vectorAddCUDA<<<grid, block>>>(dA, dB, dC, nelem);
-  cudaMemcpy(C + lo, dC, sizeof(REAL_TYPE) * (hi - lo + 1), cudaMemcpyDeviceToHost);
+  dim3 grid = {(work / block.x) + (work % block.x ? 1 : 0), 1, 1};
+  vectorAddCUDA<<<grid, block>>>(dA, dB, dC, work);
+  cudaMemcpy(C + lo, dC, sizeof(REAL_TYPE) * work, cudaMemcpyDeviceToHost);
 }
 }
